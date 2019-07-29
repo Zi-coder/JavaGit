d5a9a49ca716d60b4d87687be24c070072d995b6
Anil Kumar <akofficial238444@gmail.com>
2019-07-29 14:16:06 +0530

    Update Student.java
    Added Constructor

--- Student.java
+++ Student.java
@@ -2,6 +2,10 @@ import java.util.*;
 class Student{
 String name;
 int roll;
+  Student(String n,int r){
+  name = n;
+   roll = r;
+  }
   public void printName(){
   System.out.printlln(name);
   }
